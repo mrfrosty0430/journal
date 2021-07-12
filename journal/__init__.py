5d4252371985d3898f08c43a182a4bb747668f66
Hong Sung Jun <sjtheorange@gmail.com>
2021-07-12 14:50:00 +0000

    added forms

--- journal/__init__.py
+++ journal/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 import sys
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.scjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.gettingStarted
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = 'd2707fea9778e085491e2dbbc73ff30e'
     from .views import main_views, register_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(register_views.bp)
--- journal/views/register_views.py
+++ journal/views/register_views.py
@@ -1,23 +1,28 @@
-from flask import Blueprint,render_template
-from pymongo import MongoClient
+from flask import Flask, render_template, url_for, flash, redirect,Blueprint
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username =  StringField("아이디", 
+                            validators=[DataRequired(), Length(min=4, max=20)])
+    email =  StringField("이메일", 
+                            validators=[DataRequired(), Email()])
+    password = PasswordField("비밀번호", 
+                            validators=[DataRequired(), Length(min=4, max=20)])
+    confirm_password = PasswordField("비밀번호 확인", 
+                            validators=[DataRequired(), EqualTo("password")] )
+    submit = SubmitField("가입")
 
 bp = Blueprint('register', __name__, url_prefix='/register')
 
 
 @bp.route('/',methods=(['POST','GET']))
 def new_post():
-    client = MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.scjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    db = client.gettingStarted
-    journal = db.journal
-    
-    journal.insert_one({
-        "sleep":"00",
-        "wake":"06.5",
-        "emotions":["happy","sad","miserable","lonely","depressed"],
-        "thoughts":"just a lonely day,didn't do much"
-        
-    })
-
-    return render_template('register.html')
-
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # 알람 카테고리에 따라 부트스트랩에서 다른 스타일을 적용 (success, danger) 
+        flash(f'{form.username.data} 님 가입 완료!', 'success')
+        return redirect(url_for('main'))
+    return render_template("register.html",form=form)
  
